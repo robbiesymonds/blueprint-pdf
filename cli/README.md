abd5c47a08122de2b3b971e51284f626df4ec4e1
robbiesymonds <robbiesymonds26@gmail.com>
2023-04-07 21:16:06 +0930

    added CLI package!

--- cli/README.md
+++ cli/README.md
@@ -1,6 +1,6 @@
 # 🚧&nbsp; Blueprint  CLI
 
-The Blueprint CLI is a command line interface for generating PDFs from schematic templates. It is built on top of the [Blueprint](https://www.npmjs.com/package/blueprint-pdf) library.
+The Blueprint CLI is a command line interface for generating PDFs from schematic templates. It is built on top of the [Blueprint](https://github.com/robbiesymonds/Blueprint) library.
 
 
 ⚙️ *Looking for the Blueprint Library? [Find it here](https://www.npmjs.com/package/blueprint-pdf)!*
@@ -11,29 +11,43 @@ The Blueprint CLI is a command line interface for generating PDFs from schematic
 Blueprint CLI is available as an [npm package](https://www.npmjs.com/package/@blueprint-pdf/cli) for installation.
 
 ```sh
-yarn add -g @blueprint-pdf/cli
-# or
-npm install -g @blueprint-pdf/cli
+npm i -g @blueprint-pdf/cli
 ```
 
 ## 🚀&nbsp; Usage
 
 The CLI is a simplifed interface that allows you to interact with Blueprint without needing to spin up a client-side or server application. Simply define a template as shown below and pass it when calling the `generate` command.
 
-### Templates
-The templates for the CLI variant are slightly 
+### Specification
+Schematics are defined using a `*.js` file that exports a single object. The object must contain a `schema` property that defines the Blueprint as specified by the [Blueprint library](https://github.com/robbiesymonds/Blueprint). You can also optionally define a `data` object inside of this file that will be used as the default data source for the template.
+
+Data can also be passed via the `--data` option when calling the `generate` command. This can be either a JSON string or a path to a JSON file. For example:
+
+**Schematic File:**
+```ts
+// Path: examples/schema.js
+
+export default {
+  schema: (data) => ({
+    // ... BlueprintSchema
+  });
+  data: {
+    // ... Record<string, any>
+  },
+};
+```
 
 **Using Inline Data:**
 ```sh
-blueprint generate ./schema.json --data '{ "name": "John Doe" }'
+blueprint generate examples/schema.js --data '{ "name": "John Doe" }'
 ```
 
 **Using a Data File:**
 ```sh
-blueprint generate ./schema.json --data ./data.json
+blueprint generate examples/schema.js --data examples/data.json
 ```
 
 **Configuration Options:**
 ```sh
-blueprint generate ./schema.json --format A3 --orientation landscape
+blueprint generate examples/schema.js --format A3 --orientation landscape
 ```
--- cli/src/index.ts
+++ cli/src/index.ts
@@ -1,31 +1,97 @@
 #! /usr/bin/env node
+/// <reference types="node" />
 
 import { Command } from 'commander';
-import { BlueprintSchema } from '../../core/types';
+import * as process from 'process';
+import { join } from 'path';
+import ora from 'ora';
+import * as fs from 'fs';
 
-export function schema(schema: BlueprintSchema) {
-  return schema;
-}
+import { Blueprint } from '../..';
+import chalk from 'chalk';
 
 const program = new Command();
 
 program.version('1.0.0').description('🚧 Terminal-based utility for the blueprint-pdf package.');
 
 program
-  .command('generate <schema> <output>')
+  .command('generate <schema>')
   .description('Generate a PDF from a Blueprint schema.')
-  .option('-d, --data <data>', 'Data to use when generating the PDF.')
-  .option('-o, --output <output>', 'Output location to save the file to.')
-  .option('--orientation <orientation>', 'Orientation of the PDF to generate.')
-  .option('--format <format>', 'Format of the PDF to generate.')
-  .action((schema, output, options) => {
-    console.log('schema', schema);
-    console.log('output', output);
-    console.log('options', options);
-  });
+  .option('-d, --data <data>', 'Data to use when generating the PDF.', undefined)
+  .option('-o, --output <output>', 'Output location to save the file to.', './output.pdf')
+  .option('--orientation <orientation>', 'Orientation of the PDF to generate.', 'portrait')
+  .option('--format <format>', 'Format of the PDF to generate.', 'A4')
+  .action(handleGenerate);
 
 program.parse();
 
 if (program.args.length === 0) {
   program.help();
+}
+
+async function handleGenerate(input: string, options: Record<string, any>) {
+  const { orientation, format, data, output } = options;
+
+  const start = Date.now();
+  const s_gen = ora(chalk.dim('Reading schema file...')).start();
+
+  // Check if the schema file exists.
+  if (!fs.existsSync(join(process.cwd(), input))) {
+    s_gen.fail(chalk.red(`The schema file "${input}" does not exist!`));
+    process.exit(1);
+  }
+
+  // Dynamically import the blueprint file.
+  const mod = await import(join(process.cwd(), input)).then((m) => m.default);
+
+  // Check if the schema file exports a function.
+  if (typeof mod !== 'object' || !mod.schema) {
+    s_gen.fail(chalk.red(`The schema file "${input}" does not export a valid schematic!`));
+    process.exit(1);
+  }
+
+  s_gen.succeed();
+
+  // Data from the command line takes precedence.
+  const s_data = ora(chalk.dim('Parsing dynamic data...')).start();
+  let json: Record<string, any> | undefined = undefined;
+  json = mod.data;
+
+  if (data) {
+    try {
+      // Check if data is a JSON string.
+      if (data.startsWith('{')) {
+        json = JSON.parse(data);
+      }
+
+      // Check if data is a path to a JSON file.
+      if (fs.existsSync(join(process.cwd(), data))) {
+        json = JSON.parse(fs.readFileSync(join(process.cwd(), data), 'ascii'));
+      }
+    } catch {
+      s_data.fail(chalk.red(`The data provided is not valid JSON!`));
+      process.exit(1);
+    }
+  }
+
+  s_data.succeed();
+
+  // Generate the PDF.
+  const s_pdf = ora(chalk.dim('Generating document...')).start();
+  const pdf = new Blueprint({
+    schema: mod.schema,
+    data: json,
+    config: {
+      orientation,
+      format,
+    },
+  });
+
+  const buffer = await pdf.generate('string');
+
+  // Save the PDF.
+  fs.writeFileSync(join(process.cwd(), output), buffer);
+  s_pdf.succeed();
+
+  console.log(chalk.bold.green(`✨ Built in ${Date.now() - start}ms`));
 }
